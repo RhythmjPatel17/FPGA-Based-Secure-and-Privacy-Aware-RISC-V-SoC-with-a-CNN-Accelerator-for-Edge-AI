bc53656f04ea4c53b8b008ed959b4c87ce34b801
Rhythm Patel <rhythmpatel01017@gmail.com>
2025-08-17 13:16:04 +0530

    Create Design.v

---
+++ Source_Code/MNIST/Bram_Wrapper/Conv2D_2/Design.v
@@ -0,0 +1,53 @@
+module conv2d_64ch_output_bram_wrapper
+   (BRAM_PORTA_0_addr,
+    BRAM_PORTA_0_clk,
+    BRAM_PORTA_0_din,
+    BRAM_PORTA_0_dout,
+    BRAM_PORTA_0_en,
+    BRAM_PORTA_0_we,
+    BRAM_PORTB_0_addr,
+    BRAM_PORTB_0_clk,
+    BRAM_PORTB_0_din,
+    BRAM_PORTB_0_dout,
+    BRAM_PORTB_0_en,
+    BRAM_PORTB_0_we);
+  input [31:0]BRAM_PORTA_0_addr;
+  input BRAM_PORTA_0_clk;
+  input [31:0]BRAM_PORTA_0_din;
+  output [31:0]BRAM_PORTA_0_dout;
+  input BRAM_PORTA_0_en;
+  input [3:0]BRAM_PORTA_0_we;
+  input [31:0]BRAM_PORTB_0_addr;
+  input BRAM_PORTB_0_clk;
+  input [31:0]BRAM_PORTB_0_din;
+  output [31:0]BRAM_PORTB_0_dout;
+  input BRAM_PORTB_0_en;
+  input [3:0]BRAM_PORTB_0_we;
+
+  wire [31:0]BRAM_PORTA_0_addr;
+  wire BRAM_PORTA_0_clk;
+  wire [31:0]BRAM_PORTA_0_din;
+  wire [31:0]BRAM_PORTA_0_dout;
+  wire BRAM_PORTA_0_en;
+  wire [3:0]BRAM_PORTA_0_we;
+  wire [31:0]BRAM_PORTB_0_addr;
+  wire BRAM_PORTB_0_clk;
+  wire [31:0]BRAM_PORTB_0_din;
+  wire [31:0]BRAM_PORTB_0_dout;
+  wire BRAM_PORTB_0_en;
+  wire [3:0]BRAM_PORTB_0_we;
+
+  conv2d_64ch_output_bram conv2d_64ch_output_bram_i
+       (.BRAM_PORTA_0_addr(BRAM_PORTA_0_addr),
+        .BRAM_PORTA_0_clk(BRAM_PORTA_0_clk),
+        .BRAM_PORTA_0_din(BRAM_PORTA_0_din),
+        .BRAM_PORTA_0_dout(BRAM_PORTA_0_dout),
+        .BRAM_PORTA_0_en(BRAM_PORTA_0_en),
+        .BRAM_PORTA_0_we(BRAM_PORTA_0_we),
+        .BRAM_PORTB_0_addr(BRAM_PORTB_0_addr),
+        .BRAM_PORTB_0_clk(BRAM_PORTB_0_clk),
+        .BRAM_PORTB_0_din(BRAM_PORTB_0_din),
+        .BRAM_PORTB_0_dout(BRAM_PORTB_0_dout),
+        .BRAM_PORTB_0_en(BRAM_PORTB_0_en),
+        .BRAM_PORTB_0_we(BRAM_PORTB_0_we));
+endmodule
